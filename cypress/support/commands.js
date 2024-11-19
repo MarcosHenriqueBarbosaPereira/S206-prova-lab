98927042fe5db24e8b304cae31aeddb2d8a175fe
MarcosHenriqueBarbosaPereira <marcos.h@gec.inatel.br>
2024-11-19 17:49:41 -0300

    chore: created login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (username) => {
+    cy.get('#userSelect').select(username)
+    cy.get('form.ng-valid > .btn').click()
+  })
